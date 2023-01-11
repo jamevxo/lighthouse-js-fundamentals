e77ad84af3daf2049d0fefddcbf5604f84093cda
Jame F <j_frichitthavong@hotmail.com>
2023-01-11 01:58:33 -0500

    testing

---
+++ whichSchool.js
@@ -0,0 +1,14 @@
+//uses input of (age) to determine which school should be attended
+
+
+
+function whichSchool(age) {
+  if (age < 13) {
+    return 'Elementary School';
+  } else if (13 >= age || age <= 18) {
+    return 'Secondary School';
+  } else  {
+    return 'Lighthouse Labs';
+  }
+}
+console.log(whichSchool(19))
